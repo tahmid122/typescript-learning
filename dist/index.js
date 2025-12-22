7370c36a3765d06aad4de9f07e86a79141567ef4
Tahmid Alam <mdtahmidalam122@gmail.com>
2025-12-22 16:41:11 +0600

    explore array methods and iterations

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const arr = [1, 2, 3, 4, 5];
-console.log(arr[4]);
+const fruits = ["apple", "banana", "orange", "mango"];
+let pushed = fruits.push("watermelon");
+fruits.unshift("Guava");
+let poped = fruits.pop();
+let shifted = fruits.shift();
+console.log(fruits);
+console.log(pushed);
+console.log(poped);
+console.log(shifted);
+for (let i = 0; i <= fruits.length; i++) {
+    console.log(fruits[i]);
+}
+fruits.forEach((curVal) => console.log(curVal));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
-const arr: number[] = [1, 2, 3, 4, 5];
-console.log(arr[4]);
-const arr2 = ["a", "b"];
-const arr3: number[] = new Array(1, 2, 3, 4, 5);
-// Exploring array and its methods
+const fruits: string[] = ["apple", "banana", "orange", "mango"];
+let pushed = fruits.push("watermelon");
+fruits.unshift("Guava");
+let poped = fruits.pop();
+let shifted = fruits.shift();
+console.log(fruits);
+console.log(pushed);
+console.log(poped);
+console.log(shifted);
+
+for (let i = 0; i <= fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+fruits.forEach((curVal: string): void => console.log(curVal));
