bdf8a7fefe8e781a6ea58db7b45c5d8dd38c7fc0
Tahmid Alam <mdtahmidalam122@gmail.com>
2025-12-22 16:59:19 +0600

    explore array map and filter methods in ts

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,40 @@
 "use strict";
+// * Array and its methods and iterations
+// const fruits: string[] = ["apple", "banana", "orange", "mango"];
+// let pushed = fruits.push("watermelon");
+// fruits.unshift("Guava");
+// let poped = fruits.pop();
+// let shifted = fruits.shift();
+// console.log(fruits);
+// console.log(pushed);
+// console.log(poped);
+// console.log(shifted);
 Object.defineProperty(exports, "__esModule", { value: true });
-const fruits = ["apple", "banana", "orange", "mango"];
-let pushed = fruits.push("watermelon");
-fruits.unshift("Guava");
-let poped = fruits.pop();
-let shifted = fruits.shift();
-console.log(fruits);
-console.log(pushed);
-console.log(poped);
-console.log(shifted);
-for (let i = 0; i <= fruits.length; i++) {
-    console.log(fruits[i]);
-}
-fruits.forEach((curVal) => console.log(curVal));
+// for (let i = 0; i <= fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+// fruits.forEach((curVal: string): void => console.log(curVal));
+// * Array map and filter
+const numbers = [1, 2, 3, 4, 5];
+const double = numbers.map((num) => {
+    return num * 2;
+});
+console.log(double);
+const filteredEven = numbers.filter((number) => number % 20 === 0);
+console.log(filteredEven);
+//! Homework 1
+const names = ["tahmid", "medha"];
+const uppercase = names.map((name) => name.toUpperCase());
+console.log(uppercase);
+//! Homework 2
+const numbersArr = [1, 2, 3, 4, 5];
+const squares = numbersArr.map((num) => Math.pow(num, 2));
+console.log(squares);
+// !Homework 3
+const namesFil = ["tahmid", "medha", "etul"];
+const filteredNames = namesFil.filter((name) => name.length > 4);
+console.log(filteredNames);
+//! Homework 4
+const filterNames = ["alice", "bob"];
+const filterNamesStartA = filterNames.filter((name) => name.startsWith("a"));
+console.log(filterNamesStartA);
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,52 @@
-const fruits: string[] = ["apple", "banana", "orange", "mango"];
-let pushed = fruits.push("watermelon");
-fruits.unshift("Guava");
-let poped = fruits.pop();
-let shifted = fruits.shift();
-console.log(fruits);
-console.log(pushed);
-console.log(poped);
-console.log(shifted);
+// * Array and its methods and iterations
+// const fruits: string[] = ["apple", "banana", "orange", "mango"];
+// let pushed = fruits.push("watermelon");
+// fruits.unshift("Guava");
+// let poped = fruits.pop();
+// let shifted = fruits.shift();
+// console.log(fruits);
+// console.log(pushed);
+// console.log(poped);
+// console.log(shifted);
 
-for (let i = 0; i <= fruits.length; i++) {
-  console.log(fruits[i]);
-}
+// for (let i = 0; i <= fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
 
-fruits.forEach((curVal: string): void => console.log(curVal));
+// fruits.forEach((curVal: string): void => console.log(curVal));
+
+// * Array map and filter
+// const numbers: number[] = [1, 2, 3, 4, 5];
+// const double: number[] = numbers.map((num: number): number => {
+//   return num * 2;
+// });
+// console.log(double);
+
+// const filteredEven: number[] = numbers.filter(
+//   (number: number) => number % 20 === 0
+// );
+// console.log(filteredEven);
+
+// //! Homework 1
+// const names: string[] = ["tahmid", "medha"];
+// const uppercase: string[] = names.map((name: string) => name.toUpperCase());
+// console.log(uppercase);
+
+// //! Homework 2
+// const numbersArr: number[] = [1, 2, 3, 4, 5];
+// const squares: number[] = numbersArr.map((num: number) => num ** 2);
+// console.log(squares);
+
+// // !Homework 3
+// const namesFil: string[] = ["tahmid", "medha", "etul"];
+// const filteredNames: string[] = namesFil.filter(
+//   (name: string) => name.length > 4
+// );
+// console.log(filteredNames);
+
+// //! Homework 4
+// const filterNames: string[] = ["alice", "bob"];
+// const filterNamesStartA: string[] = filterNames.filter((name: string) =>
+//   name.startsWith("a")
+// );
+// console.log(filterNamesStartA);
